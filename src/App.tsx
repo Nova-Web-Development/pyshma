2618263fac80b1cb879ecb8d2114704a62859d66
artemgoncarov <adskayagonchaya6@gmail.com>
2023-11-13 16:17:14 +0700

    remake on React

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { useState } from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div ></div>
-    )
+  return (
+    <div className="App">
+      <div className="jumbotron text-center main">
+        <h1></h1>
+        <p>Сервис корпоративного питания для сотрудников СберБанка</p>
+        <a href="#" className="btn btn-primary btn-lg">Личный кабинет</a>
+      </div>
+      <section className="container py-5">
+        <div className="text-center">
+          <h2>Помощь</h2>
+          <p className="lead">by NovaDev</p>
+          <ul className="list-inline">
+            <li className="list-inline-item"><a href="https://vk.com/artyomjk">VK</a></li>
+            <li className="list-inline-item"><a href="https://github.com/artemgoncarov">Github</a></li>
+            <li className="list-inline-item"><a href="https://t.me/artyomjk">Telegram</a></li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default App;
