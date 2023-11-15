e0b7a0a07df073f879ce3b15d4be040cce20f574
artemgoncarov <adskayagonchaya6@gmail.com>
2023-11-15 12:20:40 +0700

    last react

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,55 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
+// import axios from 'axios';
 
-export function LoginPage() {
+export const LoginPage: React.FC = () => {
+    // const sendRequest = (url: string, params: object = {}) => {
+    //     axios.get(url)
+    // }
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+      });
+    
+      const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Ваш код обработки формы
+        console.log('Отправка данных:', formData);
+      };
+    
     return (
         <div className="container text-center form-login-reg">
             <h2>Вход</h2>
-            <form method="post">
+            <form method="post" onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Пароль:</label>
-                    <input type="password" id="password" name="password" required />
+                    <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                    />
                 </div>
                 <div className="form-group">
                     <span id="message" style={{ color: "red" }}> </span>
